50dc694c0d34706472ca3f881fdc799bbaa7891e
NinoKudava <nk1725@nyu.edu>
2019-01-21 21:48:38 -0500

    Update mySQL2.sql

--- mySQL2.sql
+++ mySQL2.sql
@@ -1,8 +1,16 @@
+
 SELECT first_name "First Name", last_name "Last Name" FROM employees;
+
 SELECT DISTINCT department_id FROM employees;
+
 SELECT * FROM employees ORDER BY first_name DESC;
+
 SELECT first_name, last_name, salary, salary*.15 AS PF FROM employees;
+
+/* The order of the SQL Clauses follows the form: 1. FROM . 2. WHERE  3. GROUP BY  4. HAVING  5. SELECT  6. ORDER BY . 7. LIMIT */
+
 SELECT employee_id, first_name, last_name, salary FROM employees ORDER BY salary ASC;
+
 SELECT MAX(salary), MIN(salary) FROM employees;
 
 SELECT AVG(salary), COUNT(*) FROM employees;
@@ -10,10 +18,13 @@ SELECT AVG(salary), COUNT(*) FROM employees;
 SELECT CONCAT(first_name,'  ', last_name) 'Employee Name' FROM employees;
 
 SELECT employee_id FROM employees LIMIT 10;
+
 SELECT first_name, last_name, salary FROM employees WHERE salary BETWEEN 10000 AND 15000 ORDER BY salary ASC;
+
 SELECT first_name, last_name, hire_date FROM employees WHERE YEAR(hire_date) LIKE '1987%';
 
 SELECT last_name FROM employees WHERE last_name = (len(last_name) = 6);
+
 SELECT last_name FROM employees WHERE last_name LIKE '------' AND first_name LIKE '--e%';
 
 SELECT job_id, COUNT(*) FROM employees GROUP BY job_id;
