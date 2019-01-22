1e2d84743515d41052cc6e3fae55b1bb99b1db50
NinoKudava <nk1725@nyu.edu>
2019-01-21 20:54:16 -0500

    Update mySQL2.sql

--- mySQL2.sql
+++ mySQL2.sql
@@ -10,3 +10,8 @@ SELECT AVG(salary), COUNT(*) FROM employees;
 SELECT CONCAT(first_name,'  ', last_name) 'Employee Name' FROM employees;
 
 SELECT employee_id FROM employees LIMIT 10;
+SELECT first_name, last_name, salary FROM employees WHERE salary BETWEEN 10000 AND 15000 ORDER BY salary ASC;
+SELECT first_name, last_name, hire_date FROM employees WHERE YEAR(hire_date) LIKE '1987%';
+
+SELECT last_name FROM employees WHERE last_name = (len(last_name) = 6);
+SELECT last_name FROM employees WHERE last_name LIKE '------' AND first_name LIKE '--e%';
